f5c74a057499c56db67b536193dc91556dace5dd
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-11 05:47:55 +0000

    cleanup

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ import random
 random_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Please guess a letter").lower()
+guess = input("Please guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-random_word_list = list(random_word)
-
-for letter in random_word_list:
+for letter in random_word:
   if letter == guess:
     print("Yes")
   else:
