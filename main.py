60efd1c918f2d19f985a33e13c4e526b65d730ab
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-11 05:45:19 +0000

    random.choice exists so i should use it

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 import random
 
-random_word = word_list[random.randint(0, (len(word_list) - 1))]
+random_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Please guess a letter").lower()
