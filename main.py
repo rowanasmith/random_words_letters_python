fdaf11e0d6678d30beb449868e0046e0c7e8d0b1
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-11 05:39:35 +0000

    Takes a guess input and makes it lowercase

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import random
 
 random_word = word_list[random.randint(0, (len(word_list) - 1))]
 
-print(random_word)
-
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Please guess a letter").lower()
 
+print(guess)
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
